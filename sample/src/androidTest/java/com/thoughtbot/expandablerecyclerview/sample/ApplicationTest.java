683fc72036a80f3e1d3f7d18143d87f0d290c258
Mohsen Rostamkolaei Motlagh <mohsen.rostamkolaeimotlagh@keepinmind.info>
2024-10-29 10:52:10 +0100

    fix the errors of the tests

--- sample/src/androidTest/java/com/thoughtbot/expandablerecyclerview/sample/ApplicationTest.java
+++ sample/src/androidTest/java/com/thoughtbot/expandablerecyclerview/sample/ApplicationTest.java
@@ -1,13 +1,23 @@
 package com.thoughtbot.expandablerecyclerview.sample;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-  public ApplicationTest() {
-    super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+  @Test
+  public void testApplicationNotNull() {
+    Application app = ApplicationProvider.getApplicationContext();
+    assertNotNull(app);
   }
 }
